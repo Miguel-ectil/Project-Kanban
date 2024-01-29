e8e61f70eca84240946b4c0fad95f284e50c846f
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2024-01-29 13:43:25 -0300

    [ADD and UPD] Aparentemente a navbar está corrigido

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,13 +1,13 @@
 
 export default function Header() {
   return (
-    <div className='fixed flex top-0 z-50 w-full items-center justify-end bg-gradient-to-t from-transparent bg-[#c80202a7] bg-slate-500 to-black p-2 px-4 transition-all lg:px-16 lg:py-4'>
-    <button
-      className="flex justify-start gap-1.5 hover:bg-[#c8020288] bg-[#c80202a7] border-2 border-red-900 rounded-lg group cursor-pointer hover:shadow-lg m-5 text-white px-1 py-0.5"
-      // onClick={handleLogoutClick}
-    >
-      <strong>SAIR</strong>
-    </button>
-  </div>
+    <div className='fixed flex w-full items-center justify-end bg-gradient-to-t from-transparent bg-[#48409E] to-indigo-900 transition-all'>
+      <button
+        className="flex hover:bg-[#aea7f3] bg-[#BFB9FF]  border-[#BFB9FF] rounded-lg cursor-pointer hover:shadow-lg m-5 text-white px-2 py-2"
+        // onClick={handleLogoutClick}
+      >
+        <strong>+ Novo Card</strong>
+      </button>
+    </div>
   )
 }
