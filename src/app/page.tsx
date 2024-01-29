46f936fbb9e62563972f4f0764c255340303607a
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2024-01-29 13:54:21 -0300

    [UPD] Corrigi o texto nos cards corrigi posição  e notei que ainda preciso melhorar a navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,30 +3,38 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <div className="flex min-h-screen flex-col items-center justify-between py-32">
+    <div className="flex min-h-screen flex-col items-center justify-between py-32 px-4">
       <div className="grid grid-cols-4 z-10 gap-8">
         <div className='border bg-[#F2F2F2] rounded-lg px-4 py-2'>
-          <strong>To do</strong>
+          <strong className="text-xl">
+            To do
+          </strong>
           <div className='border bg-[#FFFFFF] rounded-lg px-4 py-2 cursor-grabbing'>
             Testar Navegadores
           </div>
         </div>
         <div className='border bg-[#F2F2F2] rounded-lg px-4 py-2'>
-        <strong>Doing</strong>
-          <div className='border bg-[#FFFFFF] rounded-lg px-4 py-2 cursor-grabbing'>
-            Testar Navegadores
+          <strong className="text-xl">
+            Doing
+          </strong>
+          <div className='border bg-[#FFFFFF] rounded-lg px-4 py-2 text-lg cursor-grabbing'>
+            Atualizar Bibliotecas
           </div>
         </div>
         <div className='border bg-[#F2F2F2] rounded-lg px-4 py-2'>
-        <strong>QA</strong>
-          <div className='border bg-[#FFFFFF] rounded-lg px-4 py-2 cursor-grabbing'>
-            Testar Navegadores
+          <strong className="text-xl">
+            QA
+          </strong>
+          <div className='border bg-[#FFFFFF] rounded-lg px-4 py-2 text-lg cursor-grabbing'>
+          Atualizar Bibliotecas
           </div>
         </div>
         <div className='border bg-[#F2F2F2] rounded-lg px-4 py-2'>
-        <strong>Done</strong>
-          <div className='border bg-[#FFFFFF] rounded-lg px-4 py-2 cursor-grabbing'>
-            Testar Navegadores
+          <strong className="text-xl">
+            Done
+          </strong>
+          <div className='border bg-[#FFFFFF] rounded-lg px-4 py-2 text-lg cursor-grabbing'>
+            Final Project: App Development
           </div>
         </div>
       </div>
