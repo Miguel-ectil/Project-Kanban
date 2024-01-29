27e95f7ae718c56809b411e14a4c1a8a8659fde8
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2024-01-29 16:51:28 -0300

    [ADD and UPD] Adicionando 'react-icons' ao projeto, e adiconando uma logica temporaria para a parte do navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
+        <div className='fixed flex w-full items-center justify-end bg-gradient-to-t from-transparent bg-[#48409E] to-indigo-900 transition-all'>
+          <button
+            className="flex hover:bg-[#aea7f3] bg-[#BFB9FF]  border-[#BFB9FF] rounded-lg cursor-pointer hover:shadow-lg m-5 text-white px-2 py-2"
+            // onClick={handleLogoutClick}
+          >
+            <strong>+ Novo Card</strong>
+          </button>
+        </div>
         {children}
       </body>
     </html>
