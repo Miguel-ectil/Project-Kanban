e030c29478d1e0a871b65641856f707002b61290
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2024-02-14 16:05:52 -0300

    [ADD and UPD] Melhorando o modal já adiocionei o titulo os inputs e etc..

--- src/components/newCard.tsx
+++ src/components/newCard.tsx
@@ -36,10 +36,23 @@ export default function NewCard({ onClose }: any) {
                 <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                   <div className="sm:flex sm:items-start">
                     <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                      <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
-                        Deactivate account
+                      <Dialog.Title as="h1" className="text-base font-semibold leading-6 text-[#48409E]">
+                        New Card
                       </Dialog.Title>
                       <div className="mt-2">
+                        <input 
+                          type="text" 
+                          className='border p-2 rounded-lg'
+                        />
+                        <textarea 
+                          name=""
+                          id=""
+                          className='border p-2 rounded-lg mt-2'
+                          placeholder='Digite a descrição'
+                          cols="46" rows="2"
+                        >
+
+                        </textarea>
                         <p className="text-sm text-gray-500">
                           Are you sure you want to deactivate your account? All of your data will be permanently
                           removed. This action cannot be undone.
