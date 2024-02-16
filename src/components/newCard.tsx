20413b984ad6a4540288331ecac7d682ed311d21
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2024-02-16 13:34:37 -0300

    [ADD and UPD] Melhorando o modal new kanban

--- src/components/newCard.tsx
+++ src/components/newCard.tsx
@@ -34,9 +34,9 @@ export default function NewCard({ onClose }: any) {
             >
               <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
                 <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                  <div className="sm:flex sm:items-start">
-                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                      <Dialog.Title as="h1" className="text-base font-semibold leading-6 text-[#48409E]">
+                  <div className=""> {/*sm:flex sm:items-start*/}
+                    <div className="mt-3 text-center sm:ml-0 sm:mt-0 sm:text-left">
+                      <Dialog.Title as="h1" className="text-2xl font-semibold leading-6 text-[#48409E]">
                         New Card
                       </Dialog.Title>
                       <div className="mt-4">
