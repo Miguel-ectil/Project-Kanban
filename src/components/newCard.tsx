29a282455a416fd6dffcdf1fecd7043dfbd9681d
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2024-02-19 10:17:27 -0300

    [UPD] TAnsformando os campos priority do modal em botões

--- src/components/newCard.tsx
+++ src/components/newCard.tsx
@@ -77,15 +77,15 @@ export default function NewCard({ onClose }: any) {
                           <div>
                             <p className='text-xs mt-3 mb-1'>Priority</p>
                             <div className='flex space-x-3'>
-                              <p className="px-4 py-1 border border-[#FF7979] rounded-2xl text-xs text-[#FF7979] cursor-pointer">
+                              <button className="px-4 py-1 border border-[#FF7979] rounded-2xl text-xs text-[#FF7979] cursor-pointer">
                                 HIGH
-                              </p>
-                              <div className="px-4 py-1 border border-[#FFBA53] rounded-2xl text-xs text-[#FFBA53] cursor-pointer">
+                              </button>
+                              <button className="px-4 py-1 border border-[#FFBA53] rounded-2xl text-xs text-[#FFBA53] cursor-pointer">
                                 MEDIUM
-                              </div>
-                              <p className="px-4 py-1 border border-[#2BA700] rounded-2xl text-xs text-[#2BA700] cursor-pointer">
+                              </button>
+                              <button className="px-4 py-1 border border-[#2BA700] rounded-2xl text-xs text-[#2BA700] cursor-pointer">
                                 LOW
-                              </p>
+                              </button>
                             </div>
                           </div>
                         </div>
