895d031b02cc609b8396d5c5a63c40c082364717
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2024-03-06 17:29:17 -0300

    [ADD]  Adicionando imagem icon ao sidebar do projeto kanban

--- frontend/src/layout/Navbar.tsx
+++ frontend/src/layout/Navbar.tsx
@@ -2,6 +2,7 @@
 'use client'
 import React, { useState } from 'react';
 import NewCard from '@/components/newCard';
+import Image from 'next/image';
 
 export default function Header() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -11,9 +12,15 @@ export default function Header() {
   };
 
   return (
-    <div className='fixed flex w-full items-center justify-end bg-gradient-to-t from-transparent bg-[#48409E]  '>
+    <div className='fixed flex w-full items-center justify-between bg-gradient-to-t from-transparent bg-[#48409E]'>
+      <Image 
+        width={70} height={70} 
+        src='/3d_avatar_21.svg' alt={'meu logo'}
+        className='mt-1 mb-1 ml-2'
+      >
+      </Image>
       <button
-        className="flex hover:bg-[#aea7f3] bg-[#BFB9FF]  border-[#BFB9FF] rounded-lg cursor-pointer hover:shadow-lg m-5 text-white px-2 py-2"
+        className="flex hover:bg-[#aea7f3] bg-[#BFB9FF]  border-[#BFB9FF] rounded-lg cursor-pointer hover:shadow-lg mr-2 text-white px-2 py-2"
         onClick={handleNewCardClick}
       >
         <strong>+ Novo Card</strong>
