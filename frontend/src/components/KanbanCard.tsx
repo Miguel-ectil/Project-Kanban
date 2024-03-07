f3051e9da3845161a5fbb4c47d3a103faa19af73
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2024-03-07 16:26:34 -0300

    [UPD] Removendo espaço

--- frontend/src/components/KanbanCard.tsx
+++ frontend/src/components/KanbanCard.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { BiAlarm, BiCheckCircle } from 'react-icons/bi';
-
 interface KanbanCardProps {
   id: string;
   title: string;
