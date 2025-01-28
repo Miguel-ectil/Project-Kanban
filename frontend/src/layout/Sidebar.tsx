23aa80df499d6825ea64b25521ccf110c34677c4
Miguel-ectil <ectilmiguel@gmail.com>
2025-01-28 17:58:51 -0300

    [UPD] Melhorando o altura do sidebarr em relação a tela pois estava aparacendo ao abrir o modal de cadastrar

--- frontend/src/layout/Sidebar.tsx
+++ frontend/src/layout/Sidebar.tsx
@@ -4,7 +4,7 @@ import { Home, ClipboardList, Settings, LogOut } from "lucide-react";
 
 const Sidebar = () => {
   return (
-    <div className="hidden md:flex md:w-64 bg-gray-800 text-white h-full flex-col z-50">
+    <div className="hidden md:flex md:w-64 bg-gray-800 text-white h-full flex-col z-10">
       {/* Logo */}
       <div className="flex iten-center justify-center p-4 text-2xl font-bold border-b border-gray-700">
         TASKBAN
