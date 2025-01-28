1edf4041bc4268cb5df2af3fe45bd992d43bd600
Miguel-ectil <ectilmiguel@gmail.com>
2025-01-28 17:38:30 -0300

    [UPD] Melhorando os cards do kanban e adicionando funcionalidade de ocultar o sidebar ao diminuir tela para celular

--- frontend/src/layout/Sidebar.tsx
+++ frontend/src/layout/Sidebar.tsx
@@ -4,7 +4,7 @@ import { Home, ClipboardList, Settings, LogOut } from "lucide-react";
 
 const Sidebar = () => {
   return (
-    <div className="w-64 bg-gray-800 text-white h-full flex flex-col">
+    <div className="hidden md:flex md:w-64 bg-gray-800 text-white h-full flex-col">
       {/* Logo */}
       <div className="p-4 text-2xl font-bold border-b border-gray-700">
         Kanban Project
