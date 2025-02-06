9884f01d00663c9a6e4d2ff79d2b37626adab48b
Miguel-ectil <ectilmiguel@gmail.com>
2025-02-06 11:01:14 -0300

    [UPD] Corrigindo posicionamento em relação a altura do modal em tela de celular

--- src/components/newCard.tsx
+++ src/components/newCard.tsx
@@ -81,7 +81,7 @@ export default function NewCard({ onClose, id, titleTask, description, finalDate
 
   return (
     <Transition.Root show={true} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={onClose}>
+      <Dialog as="div" className=" z-10" onClose={onClose}>
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
@@ -95,7 +95,7 @@ export default function NewCard({ onClose, id, titleTask, description, finalDate
         </Transition.Child>
 
         <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div className="flex h-full items-center justify-center p-4 text-center sm:p-0">
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
