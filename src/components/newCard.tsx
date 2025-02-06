63f5be3d3bb8ed75aa236e45ecec5c391de25192
Miguel-ectil <ectilmiguel@gmail.com>
2025-02-06 11:11:16 -0300

    [UPD] Corrigindo posicionamento do modal em relação a tela de celular aumentando o tamanho dela em tela grando e aumentando o tamanho do input data

--- src/components/newCard.tsx
+++ src/components/newCard.tsx
@@ -95,7 +95,7 @@ export default function NewCard({ onClose, id, titleTask, description, finalDate
         </Transition.Child>
 
         <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-          <div className="flex h-full items-center justify-center p-4 text-center sm:p-0">
+          <div className="flex h-full items-center justify-center text-center sm:p-0">
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
@@ -105,7 +105,7 @@ export default function NewCard({ onClose, id, titleTask, description, finalDate
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-xl">
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-2xl">
                 <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                   <div className=""> {/*sm:flex sm:items-start*/}
                     <div className="mt-3 text-center sm:ml-0 sm:mt-0 sm:text-left">
@@ -148,7 +148,7 @@ export default function NewCard({ onClose, id, titleTask, description, finalDate
                             >
                               Data final 
                             </label>
-                            <div className="relative w-[16rem]">
+                            <div className="relative w-[18rem]">
                             <input
                               value={date}
                               onChange={(e) => setDate(e.target?.value)}
