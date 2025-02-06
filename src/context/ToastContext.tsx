6629506fa393681305db5d8c2a189bb17be75f02
Miguel-ectil <ectilmiguel@gmail.com>
2025-02-06 14:23:59 -0300

    [ADD] Adicionando cione as mensagens

--- src/context/ToastContext.tsx
+++ src/context/ToastContext.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { FiCheckCircle, FiAlertCircle, FiInfo } from 'react-icons/fi';
 
 interface MessageProps {
   type: 'success' | 'error' | 'info';
@@ -23,6 +24,19 @@ const Message: React.FC<MessageProps> = ({ type, message }) => {
     }
   };
 
+  const getIcon = () => {
+    switch (type) {
+      case 'success':
+        return <FiCheckCircle className="text-white text-2xl" />;
+      case 'error':
+        return <FiAlertCircle className="text-white text-2xl" />;
+      case 'info':
+        return <FiInfo className="text-white text-2xl" />;
+      default:
+        return null;
+    }
+  };
+
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsVisible(false);
@@ -34,13 +48,14 @@ const Message: React.FC<MessageProps> = ({ type, message }) => {
   return (
     isVisible && (
       <div
-        className={`fixed z-50 top-5 left-1/2 transform -translate-x-1/2 w-96 p-4 rounded-lg text-white ${getColor()} shadow-lg`}
+        className={`fixed z-50 top-5 left-1/2 transform -translate-x-1/2 w-96 p-5 rounded-lg text-white ${getColor()} shadow-lg`}
       >
-        <div className="flex justify-between items-center">
-          <p className="text-lg font-semibold">{message}</p>
+        <div className="flex items-center">
+          {getIcon()}
+          <p className="ml-4 text-lg font-semibold">{message}</p>
           <button
             onClick={() => setIsVisible(false)}
-            className="ml-2 text-white focus:outline-none"
+            className="ml-4 text-white focus:outline-none"
           >
             &times;
           </button>
